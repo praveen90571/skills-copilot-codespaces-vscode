8e71d64f827326199c64381b3330288db0adaa4e
Praveen Kumar <kumarpraveen90571@gmail.com>
2023-07-25 10:07:47 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = document.getElementById('member');
+    var skills = document.getElementById('skills');
+    var memberList = document.getElementById('memberList');
+    var skillsList = document.getElementById('skillsList');
+
+    member.style.backgroundColor = '#fff';
+    skills.style.backgroundColor = '#f5f5f5';
+    memberList.style.display = 'block';
+    skillsList.style.display = 'none';
+} 
